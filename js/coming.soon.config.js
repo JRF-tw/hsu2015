5cfc4400d8be812b78826b113dfaeb4d20c7f482
miffyC <miffy.dandelion@gmail.com>
2015-07-08 17:24:45 +0800

    7/0+7/24

--- js/coming.soon.config.js
+++ js/coming.soon.config.js
@@ -3,7 +3,7 @@
 		// Set the time at which the countdown expires.
 		// var untilDate new Date(Year, Month - 1, Day)
 		//-----------------------------------------------
-		var untilDate = new Date(2015, 7 - 1, 15);
+		var untilDate = new Date(2015, 7 - 1, 24);
 
 		$(".countdown").countdown({
 			until: untilDate, 
